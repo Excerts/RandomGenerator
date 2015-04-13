044d21b42e1420718d456d83e389c718baf16d41
class <class@CST_N417_19.cst.citytech.cuny.edu>
2015-04-13 16:36:30 -0400

    not finish yet

--- src/cst1201/RandomGenerator.java
+++ src/cst1201/RandomGenerator.java
@@ -1,16 +1,22 @@
 package cst1201;
 
+import java.util.Scanner;
+
 public class RandomGenerator {
 
     public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a lower bound: ");
+        int lower = keyboard.nextInt();
+
+        System.out.println("Enter a upper bound: ");
+        int upper = keyboard.nextInt();
+
         //TODO: Create a Scanner object to read keyboard input.
-        
         //TODO: Ask the user a lower and upper bound.
-        
         //TODO: Ask the user for the number of random integers to generate.
-        
         //TODO: Generate random integers of the desired length and bound.
-        
         //TODO: Print the average, max, min, count, and sum of the integers.
     }
 }
